590cb54044002ee7f9961608469f17ebfb873b00
Yunsu4 <ysa020428@gmail.com>
2024-11-30 22:06:51 +0900

    refactor: stream을 사용하도록 메서드 수정

--- src/main/java/pairmatching/enums/Level.java
+++ src/main/java/pairmatching/enums/Level.java
@@ -1,5 +1,7 @@
 package pairmatching.enums;
 
+import java.util.Arrays;
+
 public enum Level {
     LEVEL1("레벨1"),
     LEVEL2("레벨2"),
@@ -13,14 +15,16 @@ public enum Level {
         this.name = name;
     }
 
-    public static String isLevel(String enteredLevel) {
-        if (enteredLevel.equals(LEVEL1.name) || enteredLevel.equals(LEVEL2.name) ||
-                enteredLevel.equals(LEVEL4.name)) {
-            return enteredLevel;
-        }
-        if(enteredLevel.equals(LEVEL3.name) || enteredLevel.equals(LEVEL5.name)){
-            throw new IllegalArgumentException("[ERROR] 미션이 없는 레벨입니다.");
-        }
-        throw new IllegalArgumentException("[ERROR] 없는 레벨 입니다.");
+    public static Level getLevel(String enteredLevel) {
+        return Arrays.stream(Level.values())
+                .filter(level -> level.name.equals(enteredLevel))
+                .findAny()
+                .map(level -> {
+                    if (level == Level.LEVEL3 || level == Level.LEVEL5) {
+                        throw new IllegalArgumentException("[ERROR] 미션이 없는 레벨입니다.");
+                    }
+                    return level;
+                })
+                .orElseThrow(() -> new IllegalArgumentException("[ERROR] 없는 레벨 입니다."));
     }
 }
